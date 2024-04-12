6f6fc11a4c6d277905cbca9fed99ba9e66db0035
HTJoker <hersh.thakur@gmail.com>
2024-04-12 12:00:24 +0100

    added bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pizza_app/app.dart';
+import 'package:pizza_app/simple_bloc_oberserver.dart';
+import 'package:user_repository/user_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: Text("Hello World"),
-        ),
-      ),
-    );
-  }
+  Bloc.observer = SimpleBlocObserve();
+  runApp(MyApp(FirebaseUserRepo() as UserRepository));
 }
